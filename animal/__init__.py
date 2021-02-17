510af37eaf561a5523d82bff5d79270cbc1a4006
ruby723819 <ruby723819@gmail.com>
2021-02-17 08:22:43 +0000

    Package and Modules

---
+++ animal/__init__.py
@@ -0,0 +1,4 @@
+#animal패키지가 어떤 파일의 합인지 여기에다 적음
+
+from.cat import Cat#모듈을 가져옴   // .<- 이 폴더에 있는 cat.py이라는 파일에서 Cat이라는 클래스를 가져온다.
+from.dog import Dog
--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("x")
+from animal import dog #animal패키지에서 dog이라는 모듈을 가져온다
+from animal import cat
+
+#form animal import * #animal 패키지에서 모든 모듈을 불러온다
+
+
+d=dog.Dog()#dog모듈에서 Dog클래스를 instance로 만든다
+d.hi()
+
+c=cat.Cat()
+c.hi()  
