09b575485debd2ae1af97fce18b622c6bfc17e6c
ruby723 <molly723819@gmail.com>
2021-03-08 17:35:42 +0900

    21.3.8

---
+++ codeup_6097.py
@@ -0,0 +1,18 @@
+# 설탕과자 뽑기
+h,w=map(int,input().split())
+a=[[0]*h for _ in range(w)]
+n=int(input())
+
+for i in range(n):
+    l,d,x,y=map(int,input().split())
+
+    for j in range(l):
+        if d==0:
+            a[y-1+j][x-1]=1
+        elif d==1:
+            a[y-1][x-1+j]=1
+
+for i in range(h):
+    for j in range(w):
+        print(a[j][i],end=" ")
+    print("")
