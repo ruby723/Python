94c14236f5bdf73c9c264ebdc753f8e7cab6827e
ruby723 <molly723819@gmail.com>
2021-03-18 20:31:51 +0900

    벌집

--- Baek_2292.py
+++ Baek_2292.py
@@ -1,15 +1,9 @@
 n=int(input())
 
-a=[0]*n
-a[1]=1
-a[2]=2
+value=1
 
-for i in range(3,n):
-    a[i]=a[i-1]+(6*(i-2))
-
-for i in range(n-1,1,-1):
-    if n>=a[i]:
-        count=i
+for i in range(n):
+    value+=(6*i)
+    if value>=n:
+        print(i+1)
         break
-
-print(count)
