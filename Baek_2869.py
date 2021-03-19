7a63ed5a47939bfa4717008f402099f78c4ed4cd
ruby723 <molly723819@gmail.com>
2021-03-20 01:01:01 +0900

    달팽이

--- Baek_2869.py
+++ Baek_2869.py
@@ -1,16 +1,10 @@
+#달팽이
 import sys
 a,b,v=map(int,sys.stdin.readline().split())
 
-day=v//a
-now=a
+now=(v-a)//(a-b)
 
-while True:
-    day+=1
-    now+=(a-b)
-    if now>=v:
-        break
-    now-=b
-
-
-
-print(day)
+if (v-a)%(a-b)==0:
+    print(now+1)
+else:
+    print(now+2)
